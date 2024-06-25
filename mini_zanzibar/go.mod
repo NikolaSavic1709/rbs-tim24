e494f33ca4378e4781c0a20aa5d9a403df6353bb
Nikola1709 <savickosovska@gmail.com>
2024-06-25 15:37:58 +0200

    message

--- mini_zanzibar/go.mod
+++ mini_zanzibar/go.mod
@@ -3,6 +3,7 @@ module miniZanzibar
 go 1.22.4
 
 require (
+	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/hashicorp/consul/api v1.29.1
--- mini_zanzibar/internal/server/routes.go
+++ mini_zanzibar/internal/server/routes.go
@@ -2,14 +2,20 @@ package server
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := gin.Default()
-
+	//r.Use(cors.New(cors.Config{
+	//	AllowOrigins:     []string{"http://localhost:4200"},
+	//	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+	//	AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+	//	ExposeHeaders:    []string{"Content-Length"},
+	//	AllowCredentials: true,
+	//	MaxAge:           12 * time.Hour,
+	//}))
 	r.GET("/", s.HelloWorldHandler)
 	r.GET("/health", s.healthHandler)
 
